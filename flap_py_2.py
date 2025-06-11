de920308963f9fb418f9112bbe5ee39f3beaa390
Ray-Projects <jr888641@outlook.com>
2025-06-11 19:33:13 -0400

    Added basic loop code, and a background.

--- flap_py_2.py
+++ flap_py_2.py
@@ -1,16 +1,40 @@
 import pygame
-import os
 from sys import exit
 
-path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(path)
+# sprites
+class Sky(pygame.sprite.Sprite):
+    def __init__(self, x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("flappy-bird-assets-master/sprites/background-day.png")
+        tmp0 = self.image.width * scaling
+        tmp1 = self.image.height * scaling
+        self.image = pygame.transform.scale(self.image, (tmp0, tmp1))
+        self.size = self.image.get_size()
+        self.rect = self.image.get_rect(topleft=(x, 0))
+
+# functions
+def add_sprites():
+    skies.add(Sky(0))
+    skies.add(Sky(1024))
+
+def render_objects():
+    skies.draw(screen)
+
+def update_objects():
+    skies.update()
+
+
+# initiating variables
+pygame.init()
+screen = pygame.display.set_mode((576, 1024))
+pygame.display.set_caption('Flappy Bird')
+clock = pygame.time.Clock()
 
 # starting vars
-pygame.init()
-screen = pygame.display.set_mode((1024, 768))
-pygame.display.set_caption('Flappy Bird')
+scaling = 2
 
-clock = pygame.time.Clock()
+skies = pygame.sprite.Group()
+add_sprites()
 
 # game loop
 running = True
@@ -20,6 +44,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    if running:
+        update_objects()
+        render_objects()
 
     pygame.display.flip()
     clock.tick(60)
